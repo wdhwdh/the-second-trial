cdc9eba3ca27e5e621a81316ebacca68b02c2d36
dh <dh@dh-RV411-RV511-E3511-S3511-RV711-E3411>
2019-05-06 18:50:52 +0900

    initial commit

---
+++ src/secondtrial.java
@@ -0,0 +1,8 @@
+
+public class secondtrial {
+public static void main(String[]args) {
+	System.out.println("the second trial");
+	System.out.println("I'm trying this by myself");
+	System.out.println("Good");
+}
+}
