dd4adb69bb65faabe4035ed3a659f86dd1c8808d
dh <dh@dh-RV411-RV511-E3511-S3511-RV711-E3411>
2019-05-06 18:59:17 +0900

    second commit

--- src/secondtrial.java
+++ src/secondtrial.java
@@ -4,5 +4,6 @@ public static void main(String[]args) {
 	System.out.println("the second trial");
 	System.out.println("I'm trying this by myself");
 	System.out.println("Good");
+	System.out.println("Try it again?");
 }
 }
